f74b61325197e628b09b3a8e9ce31669c3604ecc
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-02-18 09:21:59 +0100

    Create Angular app

---
+++ apps/gymnastika/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'gymnastika',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/gymnastika',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
---
+++ apps/gymnastika/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gymnastika';
+}
