f4a2d3bb1fd67b762c63c6c04caed6c0c789d1db
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-02-28 15:29:32 +0100

    Added app dashboard

--- apps/gymnastika/src/app/app.routing.ts
+++ apps/gymnastika/src/app/app.routing.ts
@@ -7,6 +7,16 @@ export const routes: Routes = [
     component: DefaultLayoutComponent,
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/dashboard'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('@team-management/lazy/app-dashboard').then(m => m.AppDashboardModule)
+      },
+      {
         path: 'exercises',
         loadChildren: () =>
           import('@team-management/lazy/exercises').then(m => m.ExercisesModule)
---
+++ libs/feature/lazy/app-dashboard/src/lib/app-dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AppDashboardComponent } from './app-dashboard/app-dashboard.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: '', pathMatch: 'full', component: AppDashboardComponent }
+    ])
+  ],
+  declarations: [AppDashboardComponent]
+})
+export class AppDashboardModule {}
