3db187cf83f7b001bc4acbac12fe5f35f5a11fa0
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-03-03 08:26:12 +0100

    Added exercise req service

---
+++ libs/utils/http/src/lib/root-services/exercise-req.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { Exercise } from '@team-management/data/interfaces';
+import { Observable, timer } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExerciseReqService {
+  private _exercises: Exercise[] = [
+    {
+      id: 'ex1',
+      name: 'Ex 1',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catA',
+        name: 'Cat A'
+      }  
+    },
+    {
+      id: 'ex2',
+      name: 'Ex 2',
+      muscles: ['m1', 'm2', 'm3'],
+      category: {
+        id: 'catA',
+        name: 'Cat A'
+      }  
+    },
+    {
+      id: 'ex3',
+      name: 'Ex 3',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catA',
+        name: 'Cat A'
+      }  
+    },
+    {
+      id: 'ex1',
+      name: 'Ex 1',
+      muscles: ['m1'],
+      category: {
+        id: 'catB',
+        name: 'Cat B'
+      }  
+    },
+    {
+      id: 'ex2',
+      name: 'Ex 2',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catB',
+        name: 'Cat B'
+      }  
+    },
+    {
+      id: 'ex3',
+      name: 'Ex 3',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catB',
+        name: 'Cat B'
+      }  
+    },
+    {
+      id: 'ex1',
+      name: 'Ex 1',
+      muscles: ['m1', 'm2', 'm3'],
+      category: {
+        id: 'catC',
+        name: 'Cat C'
+      }  
+    },
+    {
+      id: 'ex2',
+      name: 'Ex 2',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catC',
+        name: 'Cat C'
+      }  
+    },
+    {
+      id: 'ex3',
+      name: 'Ex 3',
+      muscles: ['m1'],
+      category: {
+        id: 'catC',
+        name: 'Cat C'
+      }  
+    }
+  ];
+
+  constructor() { }
+
+  getAllExercises(catId?: string): Observable<Exercise[]> {
+    const exercises = catId ? this._exercises.filter(ex => ex.category.id === catId) : this._exercises;
+    return timer(750).pipe(mapTo(exercises));
+  }
+}
--- libs/utils/http/src/lib/root-services/index.ts
+++ libs/utils/http/src/lib/root-services/index.ts
@@ -1,1 +1,2 @@
 export * from './exercise-category-req.service';
+export * from './exercise-req.service';
