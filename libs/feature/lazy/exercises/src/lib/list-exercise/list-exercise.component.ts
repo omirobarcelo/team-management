d6aa243d9da15755f706deb0acb7e74dc19ef682
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-03-03 08:31:00 +0100

    Show and select exercises by category

--- libs/feature/lazy/exercises/src/lib/list-exercise/list-exercise.component.ts
+++ libs/feature/lazy/exercises/src/lib/list-exercise/list-exercise.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ExerciseCategoryReqService } from '@team-management/utils/http';
+import { Exercise, ExerciseCategory } from '@team-management/data/interfaces';
+import { ExerciseCategoryReqService, ExerciseReqService } from '@team-management/utils/http';
 import { Observable } from 'rxjs';
-import { ExerciseCategory } from '@team-management/data/interfaces';
 
 @Component({
   selector: 'snk-list-exercise',
@@ -10,11 +10,17 @@ import { ExerciseCategory } from '@team-management/data/interfaces';
 })
 export class ListExerciseComponent implements OnInit {
   categories$: Observable<ExerciseCategory[]>;
+  exercises$: Observable<Exercise[]>;
 
-  constructor(private _exCatReqService: ExerciseCategoryReqService) {}
+  constructor(private _exCatReqService: ExerciseCategoryReqService, private _exReqService: ExerciseReqService) {}
 
   ngOnInit(): void {
     this.categories$ = this._exCatReqService.getAllCategories();
+    this.exercises$ = this._exReqService.getAllExercises();
+  }
+
+  selectCategory(catId: string): void {
+    this.exercises$ = this._exReqService.getAllExercises(catId);
   }
 
   log(arg: any): void {
