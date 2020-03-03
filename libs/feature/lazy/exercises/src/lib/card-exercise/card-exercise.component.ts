8562274a70054565cad4aeb4d235a9ed3c040917
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-03-03 09:10:40 +0100

    Implemented exercise cards and used in list

--- libs/feature/lazy/exercises/src/lib/card-exercise/card-exercise.component.ts
+++ libs/feature/lazy/exercises/src/lib/card-exercise/card-exercise.component.ts
@@ -10,6 +10,12 @@ import { Exercise } from '@team-management/data/interfaces';
 export class CardExerciseComponent implements OnInit {
   @Input() exercise: Exercise;
 
+  get imgSrc(): string {
+    const catLetter = this.exercise.category.id.slice(-1);
+    const exNum = this.exercise.id.slice(-1);
+    return `https://raw.githubusercontent.com/omirobarcelo/team-management/initial-test/assets/ex${catLetter}${exNum}.png`;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
