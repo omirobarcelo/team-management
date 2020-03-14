9bc2cb971c59ffbcc220c3e4dab71c13c6987b2f
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-03-14 19:23:50 +0100

    Get exercises from backend

--- libs/feature/api/exercises/src/lib/controllers/exercises.controller.ts
+++ libs/feature/api/exercises/src/lib/controllers/exercises.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, InternalServerErrorException, Logger } from '@nestjs/common';
+import { ExercisesService } from '../services/exercises.service';
 
 @Controller('exercises')
-export class ExercisesController {}
+export class ExercisesController {
+  protected controllerResponse<T>(action: (resolve, reject) => void, context: string): Promise<T> {
+    return new Promise<T>(async (resolve, reject) => {
+      try {
+        action(resolve, reject);
+      } catch (error) {
+        Logger.error(error.message, error.stack, context);
+        reject(new InternalServerErrorException(error));
+      }
+    });
+  }
+
+  constructor(private readonly _service: ExercisesService) {}
+
+  @Get()
+  async getAll(): Promise<any> {
+    const action = async (resolve, reject) => {
+      resolve(this._service.getAll());
+    };
+    return this.controllerResponse<any>(action, 'ExercisesController:getAll');
+  }
+}
--- libs/feature/api/exercises/src/lib/services/exercises.service.ts
+++ libs/feature/api/exercises/src/lib/services/exercises.service.ts
@@ -1,4 +1,92 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ExercisesService {}
+export class ExercisesService {
+  private _exercises = [
+    {
+      id: 'exA1',
+      name: 'Ex A1',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catA',
+        name: 'Cat A'
+      }
+    },
+    {
+      id: 'exA2',
+      name: 'Ex A2',
+      muscles: ['m1', 'm2', 'm3'],
+      category: {
+        id: 'catA',
+        name: 'Cat A'
+      }
+    },
+    {
+      id: 'exA3',
+      name: 'Ex A3',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catA',
+        name: 'Cat A'
+      }
+    },
+    {
+      id: 'exB1',
+      name: 'Ex B1',
+      muscles: ['m1'],
+      category: {
+        id: 'catB',
+        name: 'Cat B'
+      }
+    },
+    {
+      id: 'exB2',
+      name: 'Ex B2',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catB',
+        name: 'Cat B'
+      }
+    },
+    {
+      id: 'exB3',
+      name: 'Ex B3',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catB',
+        name: 'Cat B'
+      }
+    },
+    {
+      id: 'exC1',
+      name: 'Ex C1',
+      muscles: ['m1', 'm2', 'm3'],
+      category: {
+        id: 'catC',
+        name: 'Cat C'
+      }
+    },
+    {
+      id: 'exC2',
+      name: 'Ex C2',
+      muscles: ['m1', 'm2'],
+      category: {
+        id: 'catC',
+        name: 'Cat C'
+      }
+    },
+    {
+      id: 'exC3',
+      name: 'Ex C3',
+      muscles: ['m1'],
+      category: {
+        id: 'catC',
+        name: 'Cat C'
+      }
+    }
+  ];
+
+  getAll() {
+    return this._exercises;
+  }
+}
