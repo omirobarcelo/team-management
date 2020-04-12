deb20927c5ee4da66d0f6658a3a12c8d982891a2
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-04-12 18:34:15 +0200

    Added CRUD service

--- libs/feature/api/common/src/index.ts
+++ libs/feature/api/common/src/index.ts
@@ -6,3 +6,5 @@ export * from './lib/errors/custom-validation.error';
 export * from './lib/filters/custom-exception.filter';
 
 export * from './lib/middlewares/rest-logger.middleware';
+
+export * from './lib/services/crud.service';
